cb1f40a043e357d4b34d145ae76202e886b52951
S. M Shawal Chowdhury <shafayet.shawal@gmail.com>
2023-02-01 20:29:58 +0600

    react import

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,9 +1,10 @@
+import React from "react";
 function Spinner() {
   return (
-    <div className='loadingSpinnerContainer'>
-      <div className='loadingSpinner'></div>
+    <div className="loadingSpinnerContainer">
+      <div className="loadingSpinner"></div>
     </div>
-  )
+  );
 }
 
-export default Spinner
+export default Spinner;
